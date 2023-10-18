dc47766f97cd30782d35745996b3936035689a08
JamesM813 <jrm803@gmail.com>
2023-10-18 18:51:32 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,28 @@ class Instance {
     num: number;
     title: string;
     prereq: string;
+    fall: boolean;
+    spring: boolean;
 
     constructor(num: number, title: string, prereq: string){
       this.num = num;
       this.title = title;
       this.prereq = prereq;
+      this.fall = false;
+      this.spring = false;
     }
 }
 
-const Class1 = new Instance(220,"Shit Class","CS187");
-console.log(Class1.num);
+interface _List<T> { 
+    isEmpty: () => boolean;
+    head: () => T; tail: () => List<T>; }
+    type List<T> = Readonly<_List<T>>; // makes it immutable
+    function node<T>(data: T, rest: List<T>): List<T> {
+        return { isEmpty: () => false, head: () => data, tail: () => rest }; 
+    }
+    function empty<T>(): List<T> {return { isEmpty:() => true, head:() => { throw new Error(); }, tail: () => { throw new Error(); }
+}
+}
+
+//Elective here, click to insert 
+//hashmap
