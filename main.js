8eafab34645a965a5d46db88697565e49368ce11
JamesM813 <jrm803@gmail.com>
2023-10-23 19:41:41 -0400

    Saving work 10/23
    Made basic parser that parses text file and creates class objects

--- main.js
+++ main.js
@@ -2,13 +2,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var _class = /** @class */ (function () {
-    function _class(num, title, prereq) {
-        this.major = this.major;
+    function _class(major, num, prereq, fall, spring) {
+        this.major = major;
         this.num = num;
         this.prereq = prereq;
-        this.fall = false;
-        this.spring = false;
+        this.fall = fall;
+        this.spring = spring;
     }
+    _class.prototype.getMajor = function () {
+        return this.major;
+    };
+    _class.prototype.getNum = function () {
+        return this.num;
+    };
+    _class.prototype.getPrereq = function () {
+        return this.prereq;
+    };
+    _class.prototype.getFall = function () {
+        return this.fall;
+    };
+    _class.prototype.getSpring = function () {
+        return this.spring;
+    };
+    _class.prototype.toString = function () {
+        var num = String(this.num);
+        var fall = String(this.fall);
+        var spring = String(this.spring);
+        return this.major + " " + num + " " + this.prereq + " " + fall + " " + spring;
+    };
     return _class;
 }());
 var filePath = './Classes.txt';
@@ -24,6 +45,19 @@ function readFile(filePath) {
         });
     });
 }
+function parser(data) {
+    var textByLine = data.split("\n");
+    var classList = [];
+    for (var i = 0; i < textByLine.length; i++) {
+        var val = textByLine[i].split(',');
+        var fall = val[3] === 'T';
+        var spring = val[4] === 'T';
+        var input = new _class(val[0], Number(val[1]), val[2], fall, spring);
+        console.log(input.toString());
+        classList.push(input);
+    }
+    return classList;
+}
 readFile(filePath)
     .then(function (data) {
     parser(data);
@@ -31,16 +65,5 @@ readFile(filePath)
     .catch(function (error) {
     console.error(error);
 });
-function node(data, rest) {
-    return { isEmpty: function () { return false; }, head: function () { return data; }, tail: function () { return rest; } };
-}
-function empty() {
-    return { isEmpty: function () { return true; }, head: function () { throw new Error(); }, tail: function () { throw new Error(); }
-    };
-}
-function parser(data) {
-    var textByLine = data.split("\n");
-    console.log(textByLine);
-}
 //Elective here, click to insert 
 //hashmap
