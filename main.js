51fe0637ec8b5b44584856ff3d76050d252be1e6
JamesM813 <jrm803@gmail.com>
2023-11-08 18:26:56 -0500

    Cleaned up

--- main.js
+++ main.js
@@ -18,14 +18,15 @@ function parser(data) {
     var textByLine = data.split("\n");
     var classList = [];
     for (var i = 0; i < textByLine.length; i++) {
-        var val = textByLine[i].split(',');
-        var fall = val[3] === 'T';
-        var spring = val[4] === 'T';
-        var input = new class_1._class(val[0], Number(val[1]), val[2], fall, spring);
+        var _a = textByLine[i].split(','), major = _a[0], number = _a[1], prereq = _a[2], fall = _a[3], spring = _a[4];
+        var input = new class_1._class(major, Number(number), prereq, stringToBool(fall), stringToBool(spring));
         console.log(input.toString());
         classList.push(input);
     }
     return classList;
+}
+function stringToBool(s) {
+    return (s === "T");
 }
 var filePath = './Classes.txt';
 readFile(filePath)
--- main.ts
+++ main.ts
@@ -18,14 +18,16 @@ function parser(data: String){
   const classList: _class[] = [];
 
   for(let i = 0; i < textByLine.length; i++){
-    let val = textByLine[i].split(',');
-    let fall = val[3] === 'T';
-    let spring = val[4] === 'T';
-    const input = new _class(val[0],Number(val[1]),val[2],fall,spring);
+    let [major,number,prereq,fall,spring] = textByLine[i].split(',');
+    const input = new _class(major,Number(number),prereq, stringToBool(fall), stringToBool(spring));
     console.log(input.toString());
     classList.push(input);
   }
   return classList;
+}
+
+function stringToBool(s: string): boolean{
+  return (s === "T");
 }
 
 const filePath = './Classes.txt';
